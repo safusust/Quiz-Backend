1611d9bf5ac84898759978e43be45a47f9fc6d5d
safusust <safusust@gmail.com>
2024-06-23 10:33:41 +0600

    update image

--- models/Question.js
+++ models/Question.js
@@ -1,34 +1,48 @@
 const mongoose = require('mongoose');
 
+const optionSchema = new mongoose.Schema({
+  text: {
+    type: String,
+  },
+  imageUrl: {
+    type: String,
+  },
+});
+
 const questionSchema = new mongoose.Schema({
   question: {
-    type: String,
-    required: true,
+    text: {
+      type: String,
+    },
+    imageUrl: {
+      type: String,
+    },
   },
-  optionsList: {
-    type: [String],
-    required: true,
-  },
+  optionsList: [optionSchema], 
   answer: {
-    type: String,
-    required: true,
+    text: {
+      type: String,
+    },
+    imageUrl: {
+      type: String,
+    },
   },
   subject: {
     type: String,
     enum: ['Physics 1st Paper', 'Physics 2nd Paper',
-            'Chemistry 1st Paper', 'Chemistry 2nd Paper',
-            'Math 1st Paper', 'Math 2nd Paper',
-            'Biology 1st Paper','Biology 2nd Paper',
-            'ICT', 'Bangla 1st Paper','Bangla 2nd Paper'
+           'Chemistry 1st Paper', 'Chemistry 2nd Paper',
+           'Math 1st Paper', 'Math 2nd Paper',
+           'Biology 1st Paper', 'Biology 2nd Paper',
+           'ICT', 'Bangla 1st Paper', 'Bangla 2nd Paper'
     ],
     required: true,
   },
   chapter: {
     type: String,
-    enum: ['Chapter 1','Chapter 2','Chapter 3', 'Chapter 4',
-        'Chapter 5','Chapter 6','Chapter 7', 'Chapter 8',
-        'Chapter 9', 'Chapter 10',
-],
+    enum: ['Chapter 1', 'Chapter 2', 'Chapter 3', 'Chapter 4',
+           'Chapter 5', 'Chapter 6', 'Chapter 7', 'Chapter 8',
+           'Chapter 9', 'Chapter 10',
+    ],
     required: true,
   },
 });
